126862e3b5a69edc0dc287655358cf36e638d81c
Audiointensity <audiointensity@users.noreply.github.com>
2015-08-09 10:56:16 +0200

    Added release script for windows

---
+++ create-release-windows.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 version"
+    exit
+fi
+ver=$1
+
+gcc audiointensity.c -o audiointensity.exe
+7z a audiointensity-windows-$ver.zip audiointensity.bat ffmpeg.exe audiointensity.exe "Link to VideoLAN.org - Download VLC Player here.url"

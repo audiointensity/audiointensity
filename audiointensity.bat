86785016c0b12432ae8dd04e7b2e45fd2d6dbbef
Audiointensity <audiointensity@users.noreply.github.com>
2015-08-13 20:57:06 +0200

    Fixed: audiointensity.exe was not found if in local dir

--- audiointensity.bat
+++ audiointensity.bat
@@ -15,8 +15,8 @@ if errorlevel 1 (
 
 where ffmpeg 2> NUL 1>NUL
 if errorlevel 1 (
-	SET FFMPEG=ffmpeg
- 	if not exist ffmpeg (
+	SET FFMPEG="%~dp0\ffmpeg.exe"
+ 	if not exist "%~dp0\ffmpeg.exe" (
 		SET FFMPEG=C:\Progra~2\FFmpeg\bin\ffmpeg.exe
 		if not exist C:\Progra~2\FFmpeg\bin\ffmpeg.exe (
 			SET FFMPEG=C:\Progra~1\FFmpeg\bin\ffmpeg.exe
@@ -33,7 +33,7 @@ SET args=
 set /a counter=0
 for %%f in (%*) do (
 	SET var=
-	for /f "delims= " %%t in ('call "%FFMPEG%" -i %%f -vn -acodec pcm_s16le -ar 8000 -ac 1 -f wav -y - 2^>NUL ^| audiointensity') do (
+	for /f "delims= " %%t in ('call "%FFMPEG%" -i %%f -vn -acodec pcm_s16le -ar 8000 -ac 1 -f wav -y - 2^>NUL ^| "%~dp0\audiointensity.exe"') do (
 		SET args=!args! %%f :start-time=%%t :run-time=30 
  	)
 )
